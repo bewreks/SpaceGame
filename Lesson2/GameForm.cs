82ea41204e3666cba4ba765a426bf10f96410b43
Дмитрий Соколовский <bewreks@mail.ru>
2018-06-09 19:35:31 +0300

    Move to Player file

--- Lesson2/GameForm.cs
+++ Lesson2/GameForm.cs
@@ -125,34 +125,4 @@ namespace Lesson2
             _gameThread.Start();
         }
     }
-
-    public class Player
-    {
-        private int _hp;
-        private int _score;
-
-        public int Hp => _hp;
-        public int Score => _score;
-
-        public Player()
-        {
-            _hp = 100;
-            _score = 0;
-            
-            EventManager.AddEventListener(EventManager.Events.ChangeScoreEvent, OnChangeScore);
-            EventManager.AddEventListener(EventManager.Events.ChangeEnergyEvent, OnChangeEvent);
-        }
-
-        private void OnChangeEvent(IEventArgs args)
-        {
-            _hp += (args as ChangeScoreEvent).Score;
-            Logger.Print("HP: {0}", Hp);
-        }
-
-        private void OnChangeScore(IEventArgs args)
-        {
-            _score += (args as ChangeScoreEvent).Score;
-            Logger.Print("Score: {0}", Score);
-        }
-    }
 }
---
+++ Lesson2/Player.cs
@@ -0,0 +1,35 @@
+﻿using Lesson2.Events;
+using Lesson2.Loggers;
+
+namespace Lesson2
+{
+    public class Player
+    {
+        private int _hp;
+        private int _score;
+
+        public int Hp => _hp;
+        public int Score => _score;
+
+        public Player()
+        {
+            _hp = 100;
+            _score = 0;
+            
+            EventManager.AddEventListener(EventManager.Events.ChangeScoreEvent, OnChangeScore);
+            EventManager.AddEventListener(EventManager.Events.ChangeEnergyEvent, OnChangeEvent);
+        }
+
+        private void OnChangeEvent(IEventArgs args)
+        {
+            _hp += (args as ChangeScoreEvent).Score;
+            Logger.Print("HP: {0}", Hp);
+        }
+
+        private void OnChangeScore(IEventArgs args)
+        {
+            _score += (args as ChangeScoreEvent).Score;
+            Logger.Print("Score: {0}", Score);
+        }
+    }
+}
