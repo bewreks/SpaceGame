3823e409fd0f018aeed3227731345fda8d86571c
Дмитрий Соколовский <bewreks@mail.ru>
2018-07-14 21:25:15 +0300

    Comments for EventSystem

--- Lesson2/Events/EventManager.cs
+++ Lesson2/Events/EventManager.cs
@@ -8,16 +8,31 @@ namespace Lesson2.Events
     {
         private static readonly Dictionary<string, Delegate> _handlers = new Dictionary<string, Delegate>();
         
+        /// <summary>
+        /// Добавление слушателя события
+        /// </summary>
+        /// <param name="eventType"></param>
+        /// <param name="handler"></param>
         public static void AddEventListener(string eventType, Action<T> handler)
         {
             _handlers[eventType] = Delegate.Combine(GetDelegate(eventType), handler);
         }
 
+        /// <summary>
+        /// Уделание слушателя события
+        /// </summary>
+        /// <param name="eventType"></param>
+        /// <param name="handler"></param>
         public static void RemoveEventListener(string eventType, Action<T> handler)
         {
             _handlers[eventType] = Delegate.Remove(GetDelegate(eventType), handler);
         }
 
+        /// <summary>
+        /// Отправка события с кастомными аргументами
+        /// </summary>
+        /// <param name="eventType"></param>
+        /// <param name="args"></param>
         public static void DispatchEvent(string eventType, T args)
         {
             Action<T>[] invocationList;
@@ -54,16 +69,30 @@ namespace Lesson2.Events
 
     public static class EventManager
     {
+        /// <summary>
+        /// Добавление слушателя события
+        /// </summary>
+        /// <param name="eventType"></param>
+        /// <param name="handler"></param>
         public static void AddEventListener(string eventType, Action<GameEventArgs> handler)
         {
             EventManager<GameEventArgs>.AddEventListener(eventType, handler);
         }
 
+        /// <summary>
+        /// Уделание слушателя события
+        /// </summary>
+        /// <param name="eventType"></param>
+        /// <param name="handler"></param>
         public static void RemoveEventListener(string eventType, Action<GameEventArgs> handler)
         {
             EventManager<GameEventArgs>.RemoveEventListener(eventType, handler);
         }
 
+        /// <summary>
+        /// Отправка события с базовыми аргументами
+        /// </summary>
+        /// <param name="eventType">Событие</param>
         public static void DispatchEvent(string eventType)
         {
             EventManager<GameEventArgs>.DispatchEvent(eventType, new GameEventArgs());
